03f9a215d6053bcf6c046db5416735d6f4ba0a91
Bayo <acadewole@gmail.com>
2024-01-26 16:48:31 -0600

    update project section order

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -19,22 +19,6 @@ const projects = [
   },
   {
     id: uuidv4(),
-    name: 'Pokedex Web Application',
-    desc: 'Small application utilizing React to display detailed stats on listed Pokemon',
-    img: PokedexWebApp,
-    ghlink: 'https://github.com/lojickse7en/pokedex-app',
-    wslink: 'https://lojickse7en.github.io/pokedex-app/',
-  },
-  {
-    id: uuidv4(),
-    name: 'Portfolio Website I',
-    desc: 'My first portfolio website built on HTML, CSS and Vanilla JavaScript',
-    img: HTMLPortfolio,
-    ghlink: 'https://github.com/lojickse7en/myportfolio',
-    wslink: 'https://lojickse7en.github.io/myportfolio/',
-  },
-  {
-    id: uuidv4(),
     name: 'Password Generator',
     desc: 'A simple yet very handy password generator, for when you need a password generated in a pinch! Built using CSS, HTML and Vanilla JavaScript',
     img: PasswordGenerator,
@@ -49,6 +33,23 @@ const projects = [
     ghlink: 'https://github.com/lojickse7en/brotherscattleco_nextjs',
     wslink: 'https://brotherscattleco-nextjs.vercel.app/',
   },
+  {
+    id: uuidv4(),
+    name: 'Pokedex Web Application',
+    desc: 'Small application utilizing React to display detailed stats on listed Pokemon',
+    img: PokedexWebApp,
+    ghlink: 'https://github.com/lojickse7en/pokedex-app',
+    wslink: 'https://lojickse7en.github.io/pokedex-app/',
+  },
+  {
+    id: uuidv4(),
+    name: 'Portfolio Website I',
+    desc: 'My first portfolio website built on HTML, CSS and Vanilla JavaScript',
+    img: HTMLPortfolio,
+    ghlink: 'https://github.com/lojickse7en/myportfolio',
+    wslink: 'https://lojickse7en.github.io/myportfolio/',
+  },
+
   {
     id: uuidv4(),
     name: 'Construction Website',
